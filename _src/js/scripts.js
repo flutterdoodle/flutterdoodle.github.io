eaf70ab77598612fca4a7d5261a5302331089a90
nchan0154 <channicoleelise@gmail.com>
2017-04-18 01:05:58 +0000

    add active navigation link

--- _src/js/scripts.js
+++ _src/js/scripts.js
@@ -2,3 +2,11 @@ lightbox.option({
   'showImageNumberLabel': false,
   'resizeDuration': 400
 })
+
+$(document).ready(function(){
+  var loc = window.location.pathname;
+
+  $('.nav__link').each(function() {
+    $(this).toggleClass('nav__link--active', $(this).attr('href') == loc);
+  });
+})
